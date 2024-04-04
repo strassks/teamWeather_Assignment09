429344319be9b53e1b8fe63440d2ed9ed77f2723
kaile <kaile@DESKTOP-A6BFU3H.wifi.uc.edu>
2024-04-04 11:24:02 -0400

    final commit, added documentation to weather_api module

--- teamWeather_Assignment09/mainPackage/weather_api.py
+++ teamWeather_Assignment09/mainPackage/weather_api.py
@@ -1,4 +1,17 @@
 #weather_api.py
+#Name: Kaileb strasser, Max Schiller, Josh Halbakken
+# email: strassks@mail.uc.edu, schillmx@mail.uc.edu, halbakjc@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 4/4/2024 
+# Course/Section: IS-4010
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This assignment calls and extracts data from the 
+#openweather API and prints it to the console
+
+# Brief Description of what this module does: This module defines the functions to run the data from the API
+# Citations: https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
+#https://www.instructables.com/Get-Weather-Data-Using-Python-and-Openweather-API/
+# Anything else that's relevant: N/A
 import requests
 
 class WeatherAPI:
